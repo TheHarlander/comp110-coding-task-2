16dca07478bca5f8b73a7cabcff30770d6e3d18b
TheHarlander <theharlander260@gmail.com>
2016-05-22 19:48:40 +0100

    More Deletion Of Code
    Delete.

--- SoftFox/SoftFox/Level.h
+++
@@ -1,6 +0,0 @@
-
-	//mushroomPosistion of mushroom
-	const Coordinates& getMushroomPosition() const { return mushroomPosition; }
-	int getMushroomX() const { return mushroomPosition.getX(); }
-	int getMushroomY() const { return mushroomPosition.getY(); }
-
